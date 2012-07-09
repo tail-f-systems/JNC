c2b902b4bfc5df8d8f23665eda0ad9aadfe3847a
Emil-Tail-f <emil@tail-f.com>
2012-07-09 16:00:06 +0200

    getConfig and editConfig tests

--- yangtest/test/cont/ClientTest.java
+++ yangtest/test/cont/ClientTest.java
@@ -3,11 +3,13 @@ package cont;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.tailf.inm.Element;
 import com.tailf.inm.INMException;
 import com.tailf.inm.NodeSet;
 
@@ -39,6 +41,43 @@ public class ClientTest {
         String xml = config.toXMLString();
         assertFalse("XML empty", xml.isEmpty());
         assertTrue("More than one configuration", config.size() > 1);
+    }
+
+    @Test
+    public void testEditConfig() throws IOException, INMException {
+        NodeSet config = client.getConfig();
+        Element c = null;
+        Iterator iter = config.iterator();
+        while(iter.hasNext()) {
+            c = (Element)iter.next();
+            if (c.name == "c") {
+                break;
+            }
+        }
+        Element child = new Element(c.namespace, "s");
+        child.setValue("test");
+        c.insertChild(child);
+        NodeSet newConfig = client.editConfig(c);
+        Iterator iter1 = newConfig.iterator(), iter2 = config.iterator();
+        while(iter1.hasNext()) {
+            Element elem1 = (Element)iter1.next();
+            Element elem2 = (Element)iter2.next();
+            assertTrue("Roughly equal", elem1.compare(elem2) >= 0);
+            assertTrue("Exactly equal", elem1.compare(elem2) == 0);
+            if (elem1.name.compareTo("c") == 0) {
+                Element c1 = elem1.getChild("s");
+                String value = (String) c1.getValue();
+                boolean isTest = value.compareTo("test") == 0;
+                assertTrue("Value of s in newConfig is 'test'", isTest);
+            }
+            if (elem2.name.compareTo("c") == 0) {
+                // TODO: [DRY] write function that does this check
+                Element c2 = elem2.getChild("s");
+                String value = (String) c2.getValue();
+                boolean isTest = value.compareTo("test") == 0;
+                assertFalse("Value of s in newConfig is not 'test'", isTest);
+            }
+        }
     }
 
 }
