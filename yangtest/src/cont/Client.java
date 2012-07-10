f697aa89667bb78337e5513b31e980800b6aee49
Emil-Tail-f <emil@tail-f.com>
2012-07-10 15:18:16 +0200

    Minor changes to cont.Client

--- yangtest/src/cont/Client.java
+++ yangtest/src/cont/Client.java
@@ -6,33 +6,27 @@ import com.tailf.confm.*;
 import com.tailf.inm.*;
 
 public class Client {
-    
-    private Device devRef[] = {null};
+
     private Device dev;
+    private Device devRef[] = {null};
     private DeviceUser duserRef[] = {null};
-    private DeviceUser duser;
     
     public Client() {
-        this.init();
-    }
-    
-    private void init() {
         util.Client.init(devRef, duserRef);
         dev = devRef[0];
-        duser = duserRef[0];
     }
-    
-    public NodeSet getConfig() throws IOException,INMException{
+
+    public NodeSet getConfig() throws IOException, INMException {
         return util.Client.getConfig(dev);
     }
-    
-    public NodeSet editConfig(Element config) throws IOException,INMException {
+
+    public NodeSet editConfig(Element config) throws IOException, INMException {
         return util.Client.editConfig(dev, config);
     }
-    
+
     public static void main(String[] args) throws IOException, INMException {
         Client client = new Client();
-        assert (client.dev != null): "Client: dev = null";
+        assert (client.dev != null): "Client assertion failed: dev != null";
     }
     
 }
