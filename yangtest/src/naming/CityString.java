914cba79ff760ae85a706d8a7bf98949c3593adf
Emil-Tail-f <emil@tail-f.com>
2012-07-13 15:52:43 +0200

    Add modified versions of naming classes.
    By applying these changes, the Java code compiles.

---
+++ yangtest/src/naming/CityString.java
@@ -0,0 +1,10 @@
+package naming;
+
+public class CityString extends com.tailf.confm.xs.String {
+
+    public CityString(String value) {
+        super(value);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
