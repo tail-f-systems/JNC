b60cce0e95a3b7a70682760bd8ef8145afba1614
Emil-Tail-f <emil@tail-f.com>
2012-07-13 15:51:11 +0200

    Add unmodified generated classes
    Since the yang module is designed to break the consistency of the
    generated classes, it is not surprising that they are not correct
    Java. The bugs that show in these classes are documented in bugs.txt.

---
+++ yangtest/src/naming/package-info.java
@@ -0,0 +1,25 @@
+/** * 
+ * This class hierarchy was generated from the Yang modules
+ * module "naming-test" (rev "unknown"), module "jinterface" (rev "unknown") by the <a target="_top" href="https://github.com/Emil-Tail-f/JPyang">JPyang</a> plugin of <a target="_top" href="http://code.google.com/p/pyang/">pyang</a>.
+ * The generated classes may be used to manipulate pieces of configuration data
+ * with NETCONF operations such as edit-config, delete-config and lock. These
+ * operations are typically accessed through the ConfM Java library by
+ * instantiating Device objects and setting up NETCONF sessions with real devices
+ * using a compatible YANG model.
+ * 
+ * <ul>
+ *     <li>
+ *         <a href="Naming-test.html">Naming-test</a>
+ *     </li>
+ *     <li>
+ *         <a href="Jinterface.html">Jinterface</a>
+ *     </li>
+ * </ul>
+ *
+ * @see <a target="_top" href="https://github.com/Emil-Tail-f/JPyang">JPyang project page</a>
+ * @see <a target="_top" href="ftp://ftp.rfc-editor.org/in-notes/rfc6020.txt">RFC 6020: YANG - A Data Modeling Language for the Network Configuration Protocol (NETCONF)</a>
+ * @see <a target="_top" href="ftp://ftp.rfc-editor.org/in-notes/rfc6241.txt">RFC 6241: Network Configuration Protocol (NETCONF)</a>
+ * @see <a target="_top" href="ftp://ftp.rfc-editor.org/in-notes/rfc6242.txt">RFC 6242: Using the NETCONF Protocol over Secure Shell (SSH)</a>
+ * @see <a target="_top" href="http://www.tail-f.com">Tail-f Systems</a>
+ */
+package naming;
