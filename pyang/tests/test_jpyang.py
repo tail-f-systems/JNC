1dcdd72073f5e1f1da3dafebaf626ae9af8a41d6
Emil-Tail-f <emil@tail-f.com>
2012-07-16 13:56:58 +0200

    Improve testMake_valid_identifier and test specifications

--- pyang/tests/test_jpyang.py
+++ pyang/tests/test_jpyang.py
@@ -61,7 +61,11 @@ class Test(unittest.TestCase):
         assert res == 'TeSt', 'was: ' + res
 
     def testCamelize(self):
-        """Tests special, "unlikely" cases of the camelize function"""
+        """Tests special, "unlikely" cases of the camelize function. 
+        
+        Does not test for removal of any characters other than - and .
+        
+        """
         res = jpyang.camelize('teSt')
         assert res == 'teSt', 'was: ' + res
         res = jpyang.camelize('a.weird-stringThis')
@@ -82,8 +86,10 @@ class Test(unittest.TestCase):
         assert res == 'A', 'was: ' + res
     
     def testGet_package(self):
-        """Tests that the get_package function works on all nodes in the
-        statement tree
+        """Tests the get_package function on all nodes in the statement tree.
+        
+        Perform tests on all nodes in the tree. The top level statement and its
+        immediate children should have the base package.
         
         """
         directory = self.ctx.opts.directory
@@ -100,12 +106,9 @@ class Test(unittest.TestCase):
         assert res == directory + '.l', 'was: ' + res
 
     def testPairwise(self):
-        """Tests that the pairwise function returns an iterator that includes
-        the next item also
-        
-        """
+        """Tests that the iterator includes the next item also"""
         l = [1, 2, 3]
-        
+
         # Test that the next() method returns correct values
         res = jpyang.pairwise(l)
         for i in range(len(l)):
@@ -113,7 +116,7 @@ class Test(unittest.TestCase):
                 assert res.next() == (l[i], l[i+1])
             else:
                 assert res.next() == (l[i], None)
-        
+
         # Test that next_item contains correct value during iteration
         res = jpyang.pairwise(l)
         i = 0
@@ -126,6 +129,30 @@ class Test(unittest.TestCase):
         assert i == len(l), '#iterations (should be ' + len(l) + '): ' + str(i)
 
     def testMake_valid_identifier(self):
+        """Tests conversion of statement arguments to valid Java identifiers.
+        
+        Tests that the make_valid_identifier function prepends keyword args
+        with a J and that the forbidden characters . and - are removed.
+        
+        Some simple sanity checks are performed as well. There are no tests for
+        other forbidden characters than '.' and '-'.
+        
+        """
+        # Call on statement with a lowercase arg
+        assert self.c.arg == 'c', 'was: ' + self.c.arg
+        res = jpyang.make_valid_identifier(self.c)
+        assert self.c.arg == 'c', 'was: ' + self.c.arg
+        assert res.arg == 'c', 'was: ' + res.arg
+
+        # Call on statement with arg containing hyphens
+        stmt = Statement(None, None, None, 'container', 'test-hyphen')
+        assert stmt.arg == 'test-hyphen', 'was: ' + stmt.arg
+        res = jpyang.make_valid_identifier(stmt)
+        assert stmt.arg == 'testHyphen', 'was: ' + stmt.arg
+        assert res.arg == 'testHyphen', 'was: ' + res.arg
+
+        # Call with reserved word as argument
+        assert len(jpyang.java_reserved_words) > 0
         for word in jpyang.java_reserved_words:
             stmt = Statement(None, None, None, 'leaf', word)
             assert stmt.arg == word, 'was: ' + stmt.arg
