cf0b1afbf2e57ec36dcd5ec3ba5fa95cda3c5089
Emil-Tail-f <emil@tail-f.com>
2012-07-20 12:06:54 +0200

    Add nodes to statement tree in test

--- pyang/tests/test_jpyang_method_generator.py
+++ pyang/tests/test_jpyang_method_generator.py
@@ -34,23 +34,49 @@ class Test(unittest.TestCase):
         self.ctx = Context(repo)
         self.ctx.opts = DummyOption('gen')
         
-        # Construct a statement tree rooted at m
+        ###### Construct a statement tree rooted at m
+        
+        # Module m with prefix p, container c and typedef t children
         self.m = Statement(None, None, None, 'module', arg='m')
+        self.ns = Statement(self.m, self.m, None, 'namespace', arg='ns')
         self.p = Statement(self.m, self.m, None, 'prefix', arg='root-m')
         self.c = Statement(self.m, self.m, None, 'container', arg='c')
-        self.m.substmts = [self.p, self.c]
+        self.t = Statement(self.m, self.m, None, 'typedef', arg='t')
+        self.m.substmts = [self.p, self.ns, self.c, self.t]
         
+        # list and leaf children of c
         self.l = Statement(self.m, self.c, None, 'list', arg='l')
         self.leaf = Statement(self.m, self.c, None, 'leaf', arg='leaf')
         self.c.substmts = [self.l, self.leaf]
         
-        self.key = Statement(self.m, self.l, None, 'key', arg='k')
-        self.k = Statement(self.m, self.l, None, 'leaf', arg='k')
-        self.l.substmts = [self.key, self.k]
+        # type child of t
+        self.ty = Statement(self.m, self.t, None, 'type', arg='int32')
+        self.t.substmts = [self.ty]
         
-        # Create new Method Generators for the list and container statements
+        # key and leaf children of c/l
+        self.key = Statement(self.m, self.l, None, 'key', arg='k my')
+        self.k = Statement(self.m, self.l, None, 'leaf', arg='k')
+        self.my = Statement(self.m, self.l, None, 'leaf', arg='my')
+        self.l.substmts = [self.key, self.k, self.my]
+        
+        # type child of c/leaf
+        self.leafty = Statement(self.m, self.leaf, None, 'type', arg='string')
+        self.leaf.substmts = [self.leafty]
+        
+        # type child of c/l/k
+        self.kty = Statement(self.m, self.k, None, 'type', arg='string')
+        self.k.substmts = [self.kty]
+        
+        # type child of c/l/my
+        self.myty = Statement(self.m, self.my, None, 'type', arg='t')
+        self.my.substmts = [self.myty]
+        
+        ########
+        
+        # Create new Method Generators for the top level statements
         self.cgen = jpyang.MethodGenerator(self.c, self.ctx)
         self.lgen = jpyang.MethodGenerator(self.l, self.ctx)
+        self.tgen = jpyang.MethodGenerator(self.t, self.ctx)
 
     def tearDown(self):
         """Runs after each test"""
