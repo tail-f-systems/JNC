637cdf3afae433a1ca745ff8f123ed4dc9651ca4
Emil-Tail-f <emil@tail-f.com>
2012-07-25 17:04:45 +0200

    Refactor confm.xs.String
    Add to class javadoc
    Add serialVersionUID
    Add if-case to equals(Object) method
    Rewrite wsCollapse and move to beginning of file, after equals
    Rewrite wsReplace, make it public and move to beginning as well
    Add static import of ConfMException.throwException
    Remove throwException methods
    Rewrite pattern methods to avoid code duplication
    WARNING: Some of these changes may introduce a bug, this class is used a lot by other classes,
    both in the confm library and by generated classes. The new rewritten methods have not been tested.

--- test/dom/src/com/tailf/confm/xs/String.java
+++ test/dom/src/com/tailf/confm/xs/String.java
@@ -1,6 +1,6 @@
 /*    -*- Java -*-
  *
- *  Copyright 2007 Tail-F Systems AB. All rights reserved.
+ *  Copyright 2007-2012 Tail-F Systems AB. All rights reserved.
  *
  *  This software is the confidential and proprietary
  *  information of Tail-F Systems AB.
@@ -12,6 +12,7 @@
 package com.tailf.confm.xs;
 
 import com.tailf.confm.*;
+import static com.tailf.confm.ConfMException.throwException;
 import java.io.Serializable;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -20,9 +21,18 @@ import java.util.regex.PatternSyntaxException;
  * This class implements the "xs:string" datatype from the
  * 'http://www.w3.org/2001/XMLSchema' namespace.
  * 
+ * White space collapse and replace methods, regexp pattern matchers, length
+ * assertion methods and an enumeration checker method are provided.
  */
 public class String implements Serializable {
 
+    /**
+     * Generated serial version UID, to be changed if this class is modified in
+     * a way which affects serialization. Please see:
+     * http://docs.oracle.com/javase/6/docs/platform/serialization/spec/version.html#6678
+     */
+    private static final long serialVersionUID = -5494289433972914667L;
+    
     private java.lang.String value;
 
     public String(java.lang.String value) {
@@ -30,17 +40,11 @@ public class String implements Serializable {
         check();
     }
 
-    /**
-     * Sets the value.
-     */
     public void setValue(java.lang.String value) throws ConfMException {
         this.value = value;
         check();
     }
 
-    /**
-     * Gets the value.
-     */
     public java.lang.String getValue() {
         return value;
     }
@@ -63,6 +67,8 @@ public class String implements Serializable {
     public boolean equals(Object b) {
         if (b instanceof com.tailf.confm.xs.String) {
             return equals((com.tailf.confm.xs.String) b);
+        } else if (b instanceof java.lang.String) {
+            return equals((java.lang.String) b);
         }
         return false;
     }
@@ -75,35 +81,34 @@ public class String implements Serializable {
      * come from the XML parser.
      */
     public static java.lang.String wsCollapse(java.lang.String value) {
-        // collapse will always do replace first
-        value = wsReplace(value);
-        byte[] s = value.getBytes();
-
-        // remove 0x20 at beginning and end
-        int i = 0;
-        while (i < s.length && s[i] == 0x20)
-            i++;
-        int n = s.length - 1;
-        while (n >= 0 && s[n] == 0x20)
-            n--;
-        s = new java.lang.String(s, i, n - i + 1).getBytes();
-
-        // collapse multiple 0x20
-        byte[] r = new byte[s.length];
-        boolean allow_space = true;
-        int j = 0;
-        for (i = 0; i < s.length; i++) {
-            if (s[i] == 0x20) {
-                if (allow_space) {
-                    r[j++] = s[i];
-                    allow_space = false;
-                }
-            } else {
-                r[j++] = s[i];
-                allow_space = true;
+        java.lang.String res = new java.lang.String(value);
+        
+        // Collapse multiple spaces into single spaces
+        res.replaceAll(" +", " ");
+        
+        // Remove any leading and/or trailing space
+        if (res.length() > 0) {
+            boolean hasLeadingSpace = res.charAt(0) == ' ';
+            boolean hasTrailingSpace = res.charAt(res.length() - 1) == ' ';
+            if (hasLeadingSpace && hasTrailingSpace) {
+                return res.substring(1, res.length()-1);
+            } else if (hasLeadingSpace) {
+                return res.substring(1, res.length());
+            } else if (hasTrailingSpace) {
+                return res.substring(0, res.length()-1);
             }
         }
-        return new java.lang.String(r, 0, j);
+        return res;
+    }
+
+    /**
+     * xs:whiteSpace replace. Returns a copy of value with all characters
+     * matching "[\t\n\r]" replaced by " " (a blank).
+     */
+    public static java.lang.String wsReplace(java.lang.String value) {
+        java.lang.String res = new java.lang.String(value);
+        res.replaceAll("[\t\n\r]", " ");
+        return res;
     }
 
     /** ---------- Restrictions ---------- */
@@ -112,30 +117,29 @@ public class String implements Serializable {
      * xs:pattern
      */
     protected void pattern(java.lang.String regex) throws ConfMException {
-        try {
-            throwException(!Pattern.matches(regex, value));
-        } catch (PatternSyntaxException e) {
-            throwException(true, e);
-        }
+        java.lang.String[] regexes = {regex};
+        pattern(regexes);
     }
 
     protected void pattern(java.lang.String[] regexes) throws ConfMException {
+        Object opaqueData = this;
+        boolean matches = true;
         try {
             for (int i = 0; i < regexes.length; i++)
-                if (Pattern.matches(regexes[i], value))
-                    return;
-
-            throwException(true);
+                if (!(matches = Pattern.matches(regexes[i], value)))
+                    break;
         } catch (PatternSyntaxException e) {
-            throwException(true, e);
+            opaqueData = e;
+            matches = false;
         }
+        throwException(!matches, opaqueData);
     }
 
     /**
      * xs:length
      */
     protected void length(int len) throws ConfMException {
-        throwException(value.length() != len);
+        throwException(value.length() != len, this);
     }
 
     /**
@@ -143,7 +147,7 @@ public class String implements Serializable {
      * bytes.
      */
     protected void minLength(int len) throws ConfMException {
-        throwException(value.length() < len);
+        throwException(value.length() < len, this);
     }
 
     /**
@@ -151,7 +155,7 @@ public class String implements Serializable {
      * bytes.
      */
     protected void maxLength(int len) throws ConfMException {
-        throwException(value.length() > len);
+        throwException(value.length() > len, this);
     }
 
     /**
@@ -175,45 +179,6 @@ public class String implements Serializable {
      */
     protected boolean enumeration(java.lang.String value) {
         return this.value.equals(value);
-    }
-
-    /**
-     * Assert that the value is 'false' Throw an ConfMException otherwise
-     */
-    protected void throwException(boolean v) throws ConfMException {
-        throwException(v, this);
-    }
-
-    /**
-     * Assert that the value is 'false' Throw an ConfMException otherwise
-     */
-    protected void throwException(boolean v, Object o) throws ConfMException {
-        if (v == true)
-            throw new ConfMException(ConfMException.BAD_VALUE, o);
-    }
-
-    /** ---------- package private --------- */
-
-    /**
-     * xs:whiteSpace replace. Replaces all occurances of #x9 (tab), #xA (line
-     * feed), and #xD (CR) with #x20 (space).
-     */
-    static java.lang.String wsReplace(java.lang.String value) {
-        byte[] s = value.getBytes();
-        byte[] r = new byte[s.length];
-        int j = 0;
-        for (int i = 0; i < s.length; i++) {
-            switch (s[i]) {
-            case 0xA:
-            case 0xD:
-            case 0x9:
-                r[j++] = 0x20;
-                break;
-            default:
-                r[j++] = s[i];
-            }
-        }
-        return new java.lang.String(r, 0, j);
     }
 
 }
