0066d281894db56003d8b12a98920377cd90795e
Emil-Tail-f <emil@tail-f.com>
2012-08-02 17:07:45 +0200

    Add indentityref test suite

---
+++ test/3-identityref/test/app/AllTests.java
@@ -0,0 +1,11 @@
+package app;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MainTest.class })
+public class AllTests {
+
+}
