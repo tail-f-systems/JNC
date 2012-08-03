4f288da9a481369c8430ef28bea7d74bd5c82985
Emil-Tail-f <emil@tail-f.com>
2012-08-03 14:54:49 +0200

    Add OrderedSet class.
    This is a utility/datastructure class from http://code.activestate.com/recipes/576694/
    The licence is open source MIT, meaning that the code can be used freely both in
    proprietary and open source software, so no worries. :)

--- pyang/plugins/jpyang.py
+++ pyang/plugins/jpyang.py
@@ -2356,3 +2356,73 @@ def support_add(fields=None):
         super.addChild($child);
         ''' + assignments + '''
     }'''  # TODO: '$' should be removed unless it is actually needed
+
+
+class OrderedSet(collections.MutableSet):
+    """A set of unique items that preserves the insertion order.
+
+    Created by: Raymond Hettinger
+    Version: 2009-03-19
+    Licence: http://opensource.org/licenses/MIT
+    Source: http://code.activestate.com/recipes/576694/
+
+    """
+
+    def __init__(self, iterable=None):
+        self.end = end = [] 
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[1]
+            curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:        
+            key, prev, after = self.map.pop(key)
+            prev[2] = after
+            after[1] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[2]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[2]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[1]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[1]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = next(reversed(self)) if last else next(iter(self))
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
+
+    def __del__(self):
+        self.clear()
