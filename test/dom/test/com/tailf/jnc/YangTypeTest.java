11183591e0c17c5b8607e4701ef7b4da22bf34df
Emil <emil@tailf.com>
2012-08-09 12:10:43 +0200

    Fix YangTypeTest
    Change method implementations in YangTypeDummy
    Change equal-method tests

--- test/dom/test/com/tailf/jnc/YangTypeTest.java
+++ test/dom/test/com/tailf/jnc/YangTypeTest.java
@@ -12,26 +12,29 @@ import com.tailf.jnc.YangType;
 public class YangTypeTest {
 
     private YangType<Object> objType;
-    private YangType<String> strType;
+    private YangType<Object> strType;
     private YangType<Short> shortType1;
     private YangType<Short> shortType2;
     private YangType<Long> longType;
     private YangInt32 i32;
     
-    private class YangTypeDummy<T> extends YangType<T> {
+    private class YangTypeDummy<T> extends YangBaseType<T> {
         private static final long serialVersionUID = 1L;
         public YangTypeDummy() {}
         public YangTypeDummy(String s) throws YangException { super(s); }
         public YangTypeDummy(T t) throws YangException { super(t); }
-        @Override protected T fromString(String s) { return null; }
+        @SuppressWarnings("unchecked") @Override 
+        protected T fromString(String s) { return (T)s; }
         @Override public void check() throws YangException {}
-        @Override public boolean canEqual(Object obj) { return true; }
+        @Override public boolean canEqual(Object obj) { 
+            return value == null ? false : obj instanceof YangTypeDummy;
+        }
     }
 
     @Before
     public void setUp() throws Exception {
         objType = new YangTypeDummy<Object>("7");
-        strType = new YangTypeDummy<String>();
+        strType = new YangTypeDummy<Object>();
         shortType1 = new YangTypeDummy<Short>((short)7);
         shortType2 = new YangInt16("7");
         longType = new YangTypeDummy<Long>(7L);
@@ -40,7 +43,8 @@ public class YangTypeTest {
 
     @Test
     public void testHashCode() {
-        assertTrue(objType.hashCode() == 0);
+        int hcode = objType.hashCode();
+        assertTrue(hcode + " not 7", hcode == "7".hashCode());
         assertTrue(strType.hashCode() == 0);
         assertTrue(shortType1.hashCode() == 7);
         assertTrue(shortType2.hashCode() == 7);
@@ -48,7 +52,7 @@ public class YangTypeTest {
     }
 
     @Test
-    public void testEquals() {
+    public void testEquals() throws YangException {
         assertTrue(shortType1.equals(shortType1));
         
         // YangInt16 can't equal non-number types
@@ -58,14 +62,14 @@ public class YangTypeTest {
         assertFalse(shortType1.equals(objType));
         assertFalse(objType.equals(shortType1));
         
-        assertTrue(shortType1.equals((Object)7));
-        assertTrue(shortType1.equals((Object)7L));
+        assertFalse(shortType1.equals((Object)7));
+        assertFalse(shortType1.equals((Object)7L));
         assertFalse(shortType1.equals((Object)"7"));
         assertFalse(shortType1.equals(null));
 
-        assertFalse(strType.equals((Object)"7"));
-        strType.value = "7";
-        assertTrue(strType.equals((Object)"7"));
+        assertFalse(strType.equals(objType));
+        strType.setValue("7");
+        assertTrue(strType.equals(objType));
     }
 
     @Test
@@ -73,11 +77,7 @@ public class YangTypeTest {
         assertTrue(shortType1.toString().equals("7"));
         assertTrue(shortType2.toString().equals("7"));
         assertTrue(longType.toString().equals("7"));
-    }
-    
-    @Test(expected=NullPointerException.class)
-    public void testToStringException1() {
-        assertTrue(objType.toString().equals("null"));
+        assertTrue(objType.toString().equals("7"));
     }
     
     @Test(expected=NullPointerException.class)
