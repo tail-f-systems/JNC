88539fe2feb70e0d1cf83d544f687add3e9289c6
Emil <emil@tailf.com>
2012-08-22 18:41:10 +0200

    WIP: Add rollback, fill to max-elements, etc. to Client.java

--- examples/1-simple/src/app/Client.java
+++ examples/1-simple/src/app/Client.java
@@ -98,8 +98,13 @@ public class Client {
         
         // Get (first) config with name "hosts"
         Hosts hostsConfig = getHostsConfig(configs);
+        
+        // Clone a backup configuration for rollback purposes
+        Element backup = hostsConfig.clone();
 
-        System.out.println("Initial config:\n" + hostsConfig.toXMLString());
+        String configAsXML = hostsConfig.toXMLString();
+        System.out.println("Initial config:\n" + configAsXML);
+        System.out.println("HashCode of the string: " + configAsXML.hashCode());
         
         // Increment number of servers at each host by one
         ElementChildrenIterator hostIterator = hostsConfig.hostIterator();
@@ -109,11 +114,8 @@ public class Client {
             // Internal representation of numberOfServers
             YangUInt32 numberOfServers = h.getNumberOfServersValue();
             
-            // Set value of the YangUInt32 with a Java long
+            // Set value of the with a Java long
             numberOfServers.setValue(numberOfServers.getValue() + 1);
-            
-            // Set value in local config
-            h.setNumberOfServersValue(numberOfServers);
         }
 
         // Verify the change
@@ -127,14 +129,15 @@ public class Client {
         }
         System.out.println();
         
-        // Get "enabled"-value although not always there
+        // Get "enabled"-value, using default value when not present
+        System.out.print("Hosts enable status: ");
         hostIterator = hostsConfig.hostIterator();
         while (hostIterator.hasNext()) {
             Host h = (Host) hostIterator.next();
-            System.out.println(h.getEnabledValue());
+            System.out.print("(" + h.getNameValue() + ", " +
+                    h.getEnabledValue() + "), ");
         }
-
-//        System.out.println("Incremented:\n" + hostsConfig.toXMLString());
+        System.out.println();
         
         // Add a new host
         YangString hostName = new YangString("uppsala");
@@ -145,27 +148,81 @@ public class Client {
             hostsConfig.addHost(uppsala);
             System.out.println("Host " + hostName + " added: OK");
         } catch (YangException e) {
-            System.out.println("Cannot add host " + hostName + ": Fail");
+            System.err.println("Cannot add host " + hostName + ": Fail");
         }
         
-        // Add one with same name (key collision!)
+        // Try to add one with same name (key collision!)
         try {
             hostsConfig.addHost(hostName);
-            System.out.println("Host " + hostName + " added twice: Fail");
+            System.err.println("Host " + hostName + " added twice: Fail");
         } catch (YangException e) {
             System.out.println("Cannot add host " + hostName + " twice: OK");
         }
-
-//        System.out.println("After collision:\n" + hostsConfig.toXMLString());
         
-        // Element config2 = client.editConfig(l).first();
-        // System.out.println("Resulting config:\n" + config2.toXMLString());
-        //
-        // l.setRefLeafValue("test");
-        //
-        // Element config3 = client.editConfig(l).first();
-        // System.out.println("Changed back to original config:\n" +
-        // config3.toXMLString());
+        // Fill the host list with MAX_ELEMENTS entries
+        final int MAX_ELEMENTS = 64;
+        int spaceLeft = MAX_ELEMENTS - hostsConfig.getChildren().size();
+        if (spaceLeft <= 0) {
+            System.err.println("No space for more host list entries! FAIL");
+        }
+        for(int i = 0; i < spaceLeft; i++) {
+            hostsConfig.addHost("host" + i);
+        }
+        if (hostsConfig.getChildren().size() == MAX_ELEMENTS) {
+            System.out.println("Host list contains " + MAX_ELEMENTS +
+                    " entries: OK");
+        } else {
+            System.err.println("Fill list to limit: FAIL (is limit 0?)");
+        }
+
+        // Try to add more than max-elements number of hosts
+        try {
+            hostsConfig.addHost("anotherhost");
+            System.err.println("More than " + MAX_ELEMENTS +
+                    " hosts added: Fail");
+        } catch (YangException e) {
+            System.out.println("Cannot add more than " + MAX_ELEMENTS +
+                    " hosts: OK");
+        }
+        
+        // Delete the added host list entries
+        for(int i = 0; i < spaceLeft; i++) {
+            hostsConfig.deleteHost("host" + i);
+        }
+        if (spaceLeft == MAX_ELEMENTS - hostsConfig.getChildren().size()) {
+            System.out.println("Restore number of hosts: OK");
+        } else {
+            System.err.println("Restore number of hosts: FAIL");
+        }
+
+        // Change name (key) of sthlm host to stockholm
+        Host sthlm = hostsConfig.getHost("sthlm");
+        sthlm.setNameValue("stockholm");
+        try {
+            hostsConfig.getHost("sthlm"); // This should now raise an exception
+            System.err.println("sthlm host should not exist: FAIL");
+        } catch (YangException e) {
+            hostsConfig.getHost("stockholm"); // This should be ok
+            System.out.println("sthlm host changed name to stockholm: OK");
+        }
+
+        // Edit the remote configuration and get the new one
+        NodeSet configs2 = client.editConfig(hostsConfig);
+        
+        // Get the new (changed) config with name "hosts"
+        hostsConfig = getHostsConfig(configs2);
+        String configAsXML2 = hostsConfig.toXMLString();
+//        System.out.println("New config:\n" + configAsXML2);
+        System.out.println("HashCode of the new configuration: " +
+                configAsXML2.hashCode());
+        
+        // Change back to original config
+        client.editConfig(backup);
+        NodeSet configs3 = client.getConfig();
+        hostsConfig = getHostsConfig(configs3);
+        String configAsXML3 = hostsConfig.toXMLString();
+        System.out.println("Rollback config hashCode:" +
+                configAsXML3.hashCode());
     }
 
 }
