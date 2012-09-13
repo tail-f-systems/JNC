4c7448bfc732a88a9cb48644ae60e6811af58cdd
Emil <emil@tailf.com>
2012-09-13 11:54:23 +0200

    Refactor YangBits
    Add smap and imap private members (and constructor parameters)
    Add mask as a private member
    Fix toString method using smap and imap
    Fix equals and hashCode method to take the members into account
    Fix decode method
    Remove valid method

--- jnc/src/com/tailf/jnc/YangBits.java
+++ jnc/src/com/tailf/jnc/YangBits.java
@@ -12,6 +12,8 @@
 package com.tailf.jnc;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 /**
  * Implements the built-in YANG data type "bits".
@@ -21,101 +23,164 @@ import java.math.BigInteger;
 public class YangBits extends YangBaseInt<BigInteger> {
 
     private static final long serialVersionUID = 1L;
+    
+    private String[] smap;
+    
+    private int[] imap;
+    
+    private BigInteger mask;
 
     /**
-     * Constructor using string arguments.
+     * Constructor with value as a String.
      * 
-     * @param value The value to initialize the object with, as a string.
+     * @param value The value to initialize the object with, as a Number.
      * @param mask The bit mask as a string.
+     * @param smap mapping from flag names to position values
+     * @param smap mapping from position values to flag names
      * @throws YangException If value does not match mask.
-     * @throws NumberFormatException If value or mask are not valid as numbers.
+     * @throws NumberFormatException If value is not valid as a number.
      */
-    public YangBits(String value, String mask) throws YangException {
-        this(new BigInteger(value), new BigInteger(mask));
-    }
-
-    /**
-     * Constructor using BigInteger arguments.
-     * 
-     * @param value The value to initialize the object with.
-     * @param mask The bit mask to initialize the object with.
-     * @throws YangException If value does not match mask.
-     */
-    public YangBits(BigInteger value, BigInteger mask) throws YangException {
+    public YangBits(String value, Number mask, String[] smap, int[] imap)
+        throws YangException {
         super(value);
-        check(mask);
+        YangException.throwException(smap.length != imap.length, value);
+        Utils.bigDecimalValueOf(mask).toBigIntegerExact();
+        this.smap = smap;
+        this.imap = imap;
+        setValue(value);
     }
 
     /**
-     * Value setter using string arguments.
+     * Constructor with value as a Number.
      * 
-     * @param value The value to set this object's value to, as a string.
-     * @param mask The bit mask to use, as a string.
-     * @throws YangException If value does not match mask.
-     * @throws NumberFormatException If value or mask are not valid as numbers.
-     */
-    public void setValue(String value, String mask) throws YangException {
-        setValue(new BigInteger(value), new BigInteger(mask));
-    }
-
-    /**
-     * Value setter using BigInteger arguments.
-     * 
-     * @param value The value to set this object's value to.
-     * @param mask The bit mask to use.
+     * @param value The value to initialize the object with, as a Number.
+     * @param mask The bit mask to initialize the object with.
+     * @param smap mapping from flag names to position values
+     * @param smap mapping from position values to flag names
      * @throws YangException If value does not match mask.
      */
-    public void setValue(BigInteger value, BigInteger mask)
-            throws YangException {
-        super.setValue(value);
-        check(mask);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.tailf.jnc.yang.YangInt#parse(java.lang.String)
-     */
-    @Override
-    protected BigInteger decode(String s) {
-        return new BigInteger(s);
-    }
-
-    /**
-     * Checks that the value space matches mask.
-     * 
-     * @param mask Bit mask.
-     * @throws YangException if value space does not match mask.
-     */
-    public void check(BigInteger mask) throws YangException {
-        final boolean fail = mask.or(value).compareTo(mask) != 0;
-        YangException.throwException(fail, this);
+    public YangBits(Number value, Number mask, String[] smap, int[] imap)
+        throws YangException {
+        super(Utils.bigDecimalValueOf(value).toBigIntegerExact());
+        YangException.throwException(smap.length != imap.length, value);
+        this.mask = Utils.bigDecimalValueOf(mask).toBigIntegerExact();
+        this.smap = smap;
+        this.imap = imap;
         check();
     }
 
+    /**
+     * Checks that the value matches mask.
+     * 
+     * @throws YangException if value space does not match mask.
+     */
+    @Override
+    public void check() throws YangException {
+        super.check();
+        if (mask == null) {
+            return;
+        }
+        final boolean fail = mask.or(value).compareTo(mask) != 0;
+        YangException.throwException(fail, this);
+    }
+
     /*
      * (non-Javadoc)
-     * 
-     * @see com.tailf.jnc.yang.YangType#canEqual(java.lang.Object)
+     * @see com.tailf.jnc.YangBaseType#toString()
+     */
+    @Override
+    public String toString() {
+        StringBuffer res = new StringBuffer();
+        for (int i = 0; i<imap.length; i++) {
+            BigInteger flag = BigInteger.ONE.shiftLeft(imap[i]);
+            if (!value.and(flag).equals(BigInteger.ZERO)) {
+                res.append(smap[i]);
+                if (i < imap.length - 1) {
+                    res.append(" ");
+                }
+            }
+        }
+        return res.toString();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.tailf.jnc.YangBaseInt#canEqual(java.lang.Object)
      */
     @Override
     public boolean canEqual(Object obj) {
         return (obj instanceof YangBits);
     }
-
+    
+    /*
+     * (non-Javadoc)
+     * @see com.tailf.jnc.YangBaseType#hashCode()
+     */
     @Override
-    protected YangBits cloneShallow() throws YangException {
-        return new YangBits(value.toString(), value.toString());
+    public int hashCode() {
+        int mapHashCodes = Arrays.hashCode(smap) + Arrays.hashCode(imap);
+        return super.hashCode() + mask.hashCode() + mapHashCodes;
     }
 
     /*
      * (non-Javadoc)
-     * 
-     * @see com.tailf.jnc.yang.YangInt#valid(java.lang.Number)
+     * @see com.tailf.jnc.YangBaseType#equals(java.lang.Object)
      */
     @Override
-    protected boolean valid(Number n) {
-        return n instanceof BigInteger;
+    public boolean equals(Object obj) {
+        if (!super.equals(obj)) {
+            return false;
+        }
+        YangBits other = (YangBits) obj;
+        if (!other.mask.equals(mask)
+                || other.smap.length != smap.length
+                || other.imap.length != imap.length) {
+            return false;
+        }
+        for (int i=0; i<smap.length; i++) {
+            if (!smap[i].equals(other.smap[i])
+                    || imap[i] != other.imap[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.tailf.jnc.YangBaseInt#decode(java.lang.String)
+     */
+    @Override
+    protected BigInteger decode(String s) throws NumberFormatException {
+        if (smap == null || imap == null) {
+            return null;
+        } else {
+            StringTokenizer st = new StringTokenizer(s);
+            BigInteger res = BigInteger.ZERO;
+            while (st.hasMoreTokens()) {
+                boolean found = false;
+                String tok = st.nextToken();
+                for (int i = 0; i<smap.length; i++) {
+                    if (tok.compareTo(smap[i]) == 0) {
+                        found = true;
+                        res = res.or(BigInteger.ONE.shiftLeft(imap[i]));
+                    }
+                }
+                if (!found) {
+                    throw new NumberFormatException(tok + " not found");
+                }
+            }
+            return res;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.tailf.jnc.YangBaseType#cloneShallow()
+     */
+    @Override
+    protected YangBits cloneShallow() throws YangException {
+        return new YangBits(value.toString(), mask, smap, imap);
     }
 
     /**
