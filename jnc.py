953b2d07b3e5228ef968009f7825baf74e63a4b7
Emil <emil@tailf.com>
2012-10-18 15:33:22 +0200

    Replace references to old github project with new ones

--- jnc.py
+++ jnc.py
@@ -377,14 +377,14 @@ node_stmts = module_stmts | yangelement_stmts | leaf_stmts
 
 package_info = '''/**
  * This class hierarchy was generated from the Yang module{0}
- * by the <a target="_top" href="https://github.com/Emil-Tail-f/JNC">JNC</a> plugin of <a target="_top" href="http://code.google.com/p/pyang/">pyang</a>.
+ * by the <a target="_top" href="https://github.com/tail-f-systems/JNC">JNC</a> plugin of <a target="_top" href="http://code.google.com/p/pyang/">pyang</a>.
  * The generated classes may be used to manipulate pieces of configuration data
  * with NETCONF operations such as edit-config, delete-config and lock. These
  * operations are typically accessed through the JNC Java library by
  * instantiating Device objects and setting up NETCONF sessions with real
  * devices using a compatible YANG model.
  * <p>{1}
- * @see <a target="_top" href="https://github.com/Emil-Tail-f/JNC">JNC project page</a>
+ * @see <a target="_top" href="https://github.com/tail-f-systems/JNC">JNC project page</a>
  * @see <a target="_top" href="ftp://ftp.rfc-editor.org/in-notes/rfc6020.txt">RFC 6020: YANG - A Data Modeling Language for the Network Configuration Protocol (NETCONF)</a>
  * @see <a target="_top" href="ftp://ftp.rfc-editor.org/in-notes/rfc6241.txt">RFC 6241: Network Configuration Protocol (NETCONF)</a>
  * @see <a target="_top" href="ftp://ftp.rfc-editor.org/in-notes/rfc6242.txt">RFC 6242: Using the NETCONF Protocol over Secure Shell (SSH)</a>
