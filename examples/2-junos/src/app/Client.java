0114da05ba69c64d795b38f9382c72bfa9d7da9a
Emil-Tail-f <emil@tail-f.com>
2012-10-30 18:15:02 +0100

    Add working junos client code

--- examples/2-junos/src/app/Client.java
+++ examples/2-junos/src/app/Client.java
@@ -10,25 +10,46 @@ import com.tailf.jnc.NetconfSession;
 import com.tailf.jnc.NodeSet;
 import com.tailf.jnc.SSHConnection;
 import com.tailf.jnc.SSHSession;
+import com.tailf.jnc.YangString;
 
 import gen.junosSystem.Junos;
 import gen.junosSystem.Configuration;
+import gen.junosSystem.configuration.routingOptions.AutonomousSystem;
 
+/**
+ * Client code to get the running configuration from a juniper router, copy it
+ * to the candidate configuration and then commit any changes made to it.
+ * 
+ * NetconfSession is used directly, no "Device" is used.
+ */
 public class Client {
 
-    private Device dev;
-    private DeviceUser duser;
-
-    public Client() {
-        this.init();
-    }
-
-    // Hardcoded fields
+    // Hard coded host name, user name and password
     private String emsUserName = "bobby";
     private String junosUserName = "admin";
     private String pass = "Admin99";
     private String junosHost = "olive1.lab";
+    
+    // The device and device user used by the client
+    private Device dev;
+    private DeviceUser duser;
 
+    /**
+     * Constructor, creates a connected client object.
+     * 
+     * @throws IOException
+     * @throws JNCException
+     */
+    public Client() {
+        this.init();
+    }
+
+    /**
+     * Connects to host using hard coded values
+     * 
+     * @throws IOException
+     * @throws JNCException
+     */
     private void init() {
         duser = new DeviceUser(emsUserName, junosUserName, pass);
         dev = new Device("mydev", duser, junosHost, 22);
@@ -52,19 +73,17 @@ public class Client {
     }
     
     /**
-     * Gets the first configuration element in configs with name "hosts".
-     * 
+     * Gets the first element in configs with name "configuration".
+     *
      * @param configs Set of device configuration data.
      * @return First hosts configuration, or null if none present.
      */
     public static Configuration getJunosConfiguration(NodeSet configs) {
-        Element config = configs.first();
-        if (!config.name.equals("hosts")) {
-            config = null;
-            for (Element elem : configs) {
-                if (elem.name.equals("configuration")) {
-                    config = elem;
-                }
+        Element config = null;
+        for (Element elem : configs) {
+            if (elem.name.equals("configuration")) {
+                config = elem;
+                break;
             }
         }
         return (Configuration)config;
@@ -80,37 +99,48 @@ public class Client {
         Junos.enable();
 
         // Start (NETCONF) sessions towards devices
-        SSHConnection c1 = new SSHConnection(client.junosHost, 22);
-        c1.authenticateWithPassword(client.junosUserName, client.pass);
-        SSHSession ssh1 = new SSHSession(c1);
-        NetconfSession dev1 = new NetconfSession(ssh1);
+        SSHConnection sshConn = new SSHConnection(client.junosHost, 22);
+        sshConn.authenticateWithPassword(client.junosUserName, client.pass);
+        SSHSession sshSession = new SSHSession(sshConn);
+        NetconfSession session = new NetconfSession(sshSession);
 
         // take locks on CANDIDATE datastore so that we are not interrupted
-        dev1.lock(NetconfSession.CANDIDATE);
+        session.lock(NetconfSession.CANDIDATE);
 
         // reset candidates so that CANDIDATE is an exact copy of running
-        dev1.copyConfig(NetconfSession.RUNNING, NetconfSession.CANDIDATE);
+        session.copyConfig(NetconfSession.RUNNING, NetconfSession.CANDIDATE);
 
-        // Get system configuration from dev1
+        // Get system configuration from the hpst
         NodeSet configs = client.getConfig();
-        Element sys1 = getJunosConfiguration(configs);
+        Configuration config = getJunosConfiguration(configs);
 
-        // Manipulate element trees locally
-        // TODO
+        // ========== Manipulate element trees locally
+        
+        // Change the as-number leaf value of 
+        // /configuration/routing-options/autonomous-system
+        // to 13.37
+        AutonomousSystem aSystem = (AutonomousSystem)config.getChild("routing-options").getChild("autonomous-system");
+        YangString asNumber = aSystem.getAsNumberValue();
+        asNumber.setValue("13.37");
+        
+        // ========== End of local element tree manipulation
 
         // Write back the updated element tree to the device
-        dev1.editConfig(NetconfSession.CANDIDATE, sys1);
+        session.editConfig(NetconfSession.CANDIDATE, config);
 
         // candidates are now updated
-        dev1.confirmedCommit(60);
+        session.confirmedCommit(60);
+
+        // Get the new configuration from the host
+        NodeSet newConfigs = client.getConfig();
+        Configuration newConfig = getJunosConfiguration(newConfigs);
+        System.out.println("Committed:\n" + newConfig.toXMLString());
 
         // now commit them, unlock the candidate config and close connection
         // Note: in case of error, the device will rollback within 1 min
-        dev1.commit();
-        dev1.unlock(NetconfSession.CANDIDATE);
+        session.commit();
+        session.unlock(NetconfSession.CANDIDATE);
         client.dev.close();
-        
-        System.out.println("Committed:\n" + sys1.toXMLString());
     }
 
 }
