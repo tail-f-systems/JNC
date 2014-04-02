175cc9155321fb4e57dc02c2a91b0c7bbddc08d3
alrighttheresham <damian.oneill@gmail.com>
2014-04-02 20:19:44 +0100

    Bad practice - Comparison of String parameter using == or !=

--- jnc/src/com/tailf/jnc/Prefix.java
+++ jnc/src/com/tailf/jnc/Prefix.java
@@ -37,7 +37,7 @@ public class Prefix extends Attribute implements Serializable {
     public Prefix(String name, String nsValue) {
         super(name, nsValue);
         ns = XMLNS_NAMESPACE;
-        if (name == "") {
+        if (name.equals("")) {
             qName = "xmlns";
         } else {
             qName = "xmlns:" + name;
