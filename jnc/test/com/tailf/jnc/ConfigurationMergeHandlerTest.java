6b2d70f323fbfa7a04eb4f93935c370351521507
alrighttheresham <damian.oneill@gmail.com>
2014-04-17 21:56:37 +0100

    corrected filename, removed bti header

--- jnc/test/com/tailf/jnc/ConfigurationMergeHandlerTest.java
+++ jnc/test/com/tailf/jnc/ConfigurationMergeHandlerTest.java
@@ -1,24 +1,4 @@
-/**
- *
- * Copyright (C) BTI Systems Inc. 2001-2013. All Rights Reserved.
- *
- * The information contained herein is the property of BTI Systems
- * Inc. and is strictly confidential. Except as expressly authorized
- * in writing by BTI Systems Inc., the holder shall keep all
- * information contained herein confidential, shall disclose the
- * information only to its employees with a need to know, and shall
- * protect the information, in whole or in part, from disclosure and
- * dissemination to third parties with the same degree of care it uses
- * to protect its own confidential information, but with no less than
- * reasonable care. Except as expressly authorized in writing by BTI
- * Systems Inc., the holder is granted no rights to use the
- * information contained herein.
- *
- * Unpublished. All rights reserved under the copyright laws of
- * Canada.
- *
- * $Id: NetconfSessionTest.java 14050 2014-04-10 16:42:03Z mliggett $
- */
+
 package com.tailf.jnc;
 
 import com.tailf.jnc.Attribute;
