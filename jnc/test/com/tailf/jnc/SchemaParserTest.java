bfbde8a6201493b9e68bbe3624b75bf0b2a5b591
alrighttheresham <damian.oneill@gmail.com>
2014-04-17 18:15:03 +0100

    added unit test support to build file, include dependencies in the lib folder so that the build is self contained

--- jnc/test/com/tailf/jnc/SchemaParserTest.java
+++ jnc/test/com/tailf/jnc/SchemaParserTest.java
@@ -9,18 +9,18 @@ import org.junit.Test;
 
 
 public class SchemaParserTest {
-    
+
     private static SchemaParser parser;
-    private HashMap<Tagpath, SchemaNode> h = new HashMap<Tagpath, SchemaNode>();      
-    
+    private HashMap<Tagpath, SchemaNode> h = new HashMap<Tagpath, SchemaNode>();
+
     public SchemaParserTest() {
     }
-    
+
     @Before
     public void setUp() throws JNCException {
         parser = new SchemaParser();
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -29,10 +29,10 @@ public class SchemaParserTest {
     public void testFileNotFound() throws JNCException {
         parser.findAndReadFile("File Not Found.schema", h, SchemaParser.class);
     }
-    
+
     @Test
     public void testLoadSchemaFromClasspath() throws JNCException {
-        parser.findAndReadFile("Yang.schema", h, SchemaParser.class);
+        parser.findAndReadFile("/Yang.schema", h, SchemaParser.class);
         Assert.assertEquals(1, h.size());
         Assert.assertEquals("urn:ietf:params:xml:ns:yang:ietf-yang-types", h.values().iterator().next().namespace);
     }
