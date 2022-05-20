1696093021405b7b49d69fa3cd368747c051ab64
Michal Novak <micnovak@cisco.com>
2022-05-20 13:29:09 +0200

    JNC - JncPyangTask for all 2 examples
    Signed-off-by: Michal Novak <micnovak@cisco.com>

---
+++ examples/buildSrc/src/main/groovy/com/tailf/jnc/gradle/JncPyangTask.groovy
@@ -0,0 +1,34 @@
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+
+//https://tomgregory.com/gradle-task-inputs-and-outputs/
+class JncPyangTask extends DefaultTask {
+    @OutputDirectory
+    File outputDir;
+
+    @Optional
+    @Input
+    String yangPath = null
+
+    @Optional
+    @Input
+    String pluginDir = null
+
+    @InputFiles
+    File[] inputFiles;
+
+    @TaskAction
+    def generate() {
+        // TODO check pyang is installed
+        inputFiles.each { yang ->
+            def script = "pyang${yangPath == null ? "" : " -p $yangPath"}${pluginDir == null ? "" : " --plugindir $pluginDir"} -f jnc --jnc-output ${outputDir} ${yang}".execute()
+            script.waitForProcessOutput(System.out, System.err)
+            logger.info "Generated ${yang}"
+        }
+        logger.info "Generated ${inputFiles.size()} to ${outputDir.getPath()} path ${yangPath}"
+    }
+}
